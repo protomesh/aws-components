d7cbccd4fb59028af70bf098d60bbb89c315d0fd
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2023-06-18 20:04:16 -0300

    Add output

--- terraform/ecs-application/outputs.tf
+++ terraform/ecs-application/outputs.tf
@@ -1,14 +1,12 @@
 output "nlb_dns_name" {
     value = length(module.nlb) > 0 ? module.nlb[0].lb_dns_name : null
-    description = "NLB for front-proxy DNS name"
-
+    description = "NLB for DNS name"
 }
 
 output "task_role_arn" {
     value = module.task_role.iam_role_arn
     description = "Task role arn"
 }
-
 
 output "application_private_domain" {
     value = "${var.name}.${var.application_account.workspace_private_domain}"
