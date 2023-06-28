0a3f50b1d5c72ef64a40d23f17141756df446598
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2023-06-28 09:11:54 -0300

    Add egress policies for capacity provider

--- terraform/ecs-cluster/capacity_providers.tf
+++ terraform/ecs-cluster/capacity_providers.tf
@@ -17,7 +17,7 @@ module "capacity_providers" {
   max_size                = lookup(each.value["autoscaling_group"], "max_size", 2)
   desired_capacity        = lookup(each.value["autoscaling_group"], "desired_capacity", 1)
   ssh_key_name            = lookup(each.value["autoscaling_group"], "ssh_key_name", null)
-  egress_with_cidr_blocks = lookup(each.value["autoscaling_group"], "egress_with_cidr_blocks", [])
+  egress_with_cidr_blocks = lookup(each.value, "egress_with_cidr_blocks", [])
 
   app_account = var.app_account
 
