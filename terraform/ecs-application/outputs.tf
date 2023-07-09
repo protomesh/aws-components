387a022f318f6ffeb5075dbe70b7e666ab4fc19e
Victor França Lopes <18534956+vflopes@users.noreply.github.com>
2023-07-09 17:25:45 -0300

    Add service private domains output

--- terraform/ecs-application/outputs.tf
+++ terraform/ecs-application/outputs.tf
@@ -1,14 +1,23 @@
 output "nlb_dns_name" {
-    value = length(module.nlb) > 0 ? module.nlb[0].lb_dns_name : null
-    description = "NLB for DNS name"
+  value       = length(module.nlb) > 0 ? module.nlb[0].lb_dns_name : null
+  description = "NLB for DNS name"
 }
 
 output "task_role_arn" {
-    value = module.task_role.iam_role_arn
-    description = "Task role arn"
+  value       = module.task_role.iam_role_arn
+  description = "Task role arn"
 }
 
 output "application_private_domain" {
-    value = "${var.name}.${var.app_account.private_dns_zone}"
-    description = "Domain for this service"
+  value       = "${var.name}.${var.app_account.private_dns_zone}"
+  description = "Domain for this service"
+}
+
+locals {
+  service_private_domains = [for k, v in local.sd_container : "${var.name}-${k}.${var.app_account.private_dns_zone}"]
+}
+
+output "service_private_domains" {
+  value       = local.service_private_domains
+  description = "Domain for this service"
 }
